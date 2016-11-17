00138ecce974bc97eb8110e30ee09548b70b4317
Nicolas <nicog.035@gmail.com>
2016-11-17 09:59:22 -0500

    add project to git

---
+++ ParkMyCar/app/src/main/java/com/bignerdranch/android/parkmycar/LocatrFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.parkmycar;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by Nicolas on 17/11/2016.
+ */
+public class LocatrFragment extends SupportMapFragment {
+}
